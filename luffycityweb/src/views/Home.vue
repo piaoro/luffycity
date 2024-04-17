5a1e823c73d380c5a34fb16f6cb08170c60688cc
power <3127609304@qq.com>
2024-04-17 15:37:29 +0800

    feature:axios配置

--- luffycityweb/src/views/Home.vue
+++ luffycityweb/src/views/Home.vue
@@ -9,6 +9,21 @@
 <script setup>
   import Header from "../components/Header.vue"
   import Footer from "../components/Footer.vue"
+  import http from "../utils/https.js"
+
+  // import axios from "axios"
+  // const http = axios.create()
+  // http.get("http://api.luffycity.cn:8000/home/get").then(resp=>{
+  //   console.log(resp.data);
+  // },(error)=>{
+  //   console.log("服务端响应错误");
+  //   return Promise.reject(error);
+  // })
+
+
+  http.get("/home/get").then(resp=>{
+    console.log(resp.data);
+  })
 </script>
 
 <style scoped>
