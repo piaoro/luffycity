b809bfaab21f8d1661dda2d9546a52c9cfced76d
power <3127609304@qq.com>
2024-05-08 16:53:43 +0800

    feature:服务端实现获取用户所有优惠券与本次下单的可用优惠券列表

--- luffycityapi/luffycityapi/apps/coupon/urls.py
+++ luffycityapi/luffycityapi/apps/coupon/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-
+    path("",views.CouponListAPIView.as_view()),
+    path("enable/",views.EnableCouponListAPIView.as_view())
 ]
--- luffycityapi/luffycityapi/apps/coupon/views.py
+++ luffycityapi/luffycityapi/apps/coupon/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .service import get_user_coupon_list, get_user_enable_coupon_list
+
 
 # Create your views here.
+class CouponListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """获取用户拥有的所有优惠券"""
+        user_id = request.user.id
+        coupon_data = get_user_coupon_list(user_id)
+        return Response(coupon_data)
+
+
+class EnableCouponListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """获取用户本次拥有的本次下单可用所有优惠券"""
+        user_id = request.user.id
+        coupon_data = get_user_enable_coupon_list(user_id)
+        return Response(coupon_data)
